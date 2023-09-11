743f4a2bbf578fe8ed4fc74ac6c67e98effc224f
Kenniusboggs <ogbogukenny@yahoo.com>
2023-09-11 11:33:27 +0100

    Mobile resp. for login and register pages

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,24 +16,34 @@ function Login() {
         <>
             <div className="relative flex justify-center items-center overflow-hidden w-full h-[100vh]">
                 
-                <div className="flex justify-start items-center w-full">
-                    <div className="w-[700px] h-[700px] mt-[50px]">
+                <div className="flex justify-start items-center overflow-hidden w-full 
+                    md:mt-0 sm:-mt-[200px] xs:mt-0 xxs:-mt-[27%] -mt-[10%]">
+
+                    <div className="md:w-[50%] md:h-[50%] sm:w-[90%] sm:h-[90%] xs:w-[95%] xs:h-[95%] 
+                        w-[98%] h-[98%] md:ml-0 sm:-ml-[200px] ml-0">
                         <img src={logo} alt="logo" className="w-full h-full opacity-20"/>
                     </div>
+                    
                 </div>
 
-                <div className="flex flex-col justify-center items-center w-full absolute z-[2] top-[15%]">
+                <div className="flex flex-col justify-center items-center w-full absolute z-[2] xs:top-[15%] 
+                    top-[5%]">
 
-                    <div className="font-poppins flex flex-col justify-center items-center">
-                        <h1 className="text-[45px] font-bold text-center tracking-tight">
-                            Shosan Computer Based Test
+                    <div className="font-poppins flex flex-col justify-center xs:items-center items-start 
+                        xs:w-full w-[80%]">
+                        <h1 className="sm:text-[45px] xxs:text-[30px] text-[25px] font-bold xs:text-center 
+                            text-start tracking-tight">
+                            Shosan Computer <br className="xs:hidden block"/> Based Test
                         </h1>
-                        <h3 className="text-[26px] font-semibold text-center tracking-tight mt-[20px]">
-                            Welcome to your Computer Based Test
+                        <h3 className="sm:text-[26px] xs:text-[20px] xxs:text-[22px] text-[20px] font-semibold 
+                            xs:text-center text-start xs:tracking-tight tracking-normal xs:leading-none 
+                            leading-[25px] xxs:mt-[20px] mt-[10px]">
+                            Welcome to your <br className="xs:hidden block"/>Computer Based Test
                         </h3>
                     </div>
 
-                    <div className="font-bold font-poppins text-[30px] my-[50px] text-white navText1">
+                    <div className="font-bold font-poppins md:text-[30px] sm:text-[40px] text-[30px] w-[80%]
+                        xs:my-[50px] my-[30px] xs:text-center text-start text-white navText1">
                         Login
                     </div>
 
@@ -41,16 +51,17 @@ function Login() {
 
                         <form 
                             name="cbtForm" 
-                            className="flex flex-col justify-center items-center w-[30%]">
+                            className="flex flex-col justify-center items-center md:w-[30%] sm:w-[40%] 
+                                xs:w-[60%] w-[80%]">
 
-                            <div className="mb-2 w-full">
+                            <div className="xs:mb-2 mb-[15px] w-full">
                                 <input 
                                     type="email" 
                                     placeholder="Email address" 
                                     name="emailForm" 
                                     className="w-full text-[16px]"/>
                             </div>
-                            <div className="mb-3 w-full">
+                            <div className="xs:mb-3 mb-[10px] w-full">
                                 <input 
                                     type="password" 
                                     placeholder="Password" 
@@ -58,7 +69,7 @@ function Login() {
                                     className="w-full text-[16px]"/>
                             </div>
 
-                            <div className="text-[16px]">
+                            <div className="xxs:text-[16px] text-[14px] xxs:tracking-normal tracking-tighter">
                                 Don&apos;t have an account,&nbsp;
                                 <span onClick={()=>Navigate("/register")} 
                                     className="text-blue-600 font-semibold cursor-pointer">
